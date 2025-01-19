e6e77ac864a002f7c627e61ed254ecc580f24b08
javen2015 <zqlin15@gmail.com>
2025-01-19 08:20:08 +0000

    Jan 19, 2025, 8:20 AM

--- Code/Code.py
+++ Code/Code.py
@@ -1,4 +1,4 @@
 Answer = input('Do you like never gonna give you up? Type yes or no.')
 if Answer == "yes":
     print("Okay")
-    Answer2 = input("Anything else you")
+    Answer2 = input("Do you want to shaything else?")
