662cc40cbc2ff69c88c3786e8967713568e18db4
javen2015 <zqlin15@gmail.com>
2025-02-09 08:23:36 +0000

    Feb 9, 2025, 8:23 AM

--- CT06_05/lesson5.py
+++ CT06_05/lesson5.py
@@ -1,3 +1,3 @@
 Name = input('Who is the birthday person?')
 Age = input('How old will ', Name , ' be this year?')
-Message = input('What is your personal message to', Name,'?')
+Message = input('What is your personal message to ', Name,'?')
