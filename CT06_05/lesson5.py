23bb1d7c28aa88c98e7b359586ca356c2cc2b489
javen2015 <zqlin15@gmail.com>
2025-02-09 08:46:40 +0000

    Feb 9, 2025, 8:46 AM

--- CT06_05/lesson5.py
+++ CT06_05/lesson5.py
@@ -1,16 +1,15 @@
-Name = input('Who is the birthday person?')
-Age = int(input('How old will he or she be this year?'))
-Message = input('What is your personal message to him or her?')
-if Age == 11 or Age == 12:
-    print('Happy',Age,'th Birthday,',Name,'!',Message)
-elif Age % 10 == 1 :
-    print('Happy',Age,'st Birthday,',Name,'!',Message)
-elif Age % 10 == 2:
-    print('Happy',Age,'nd Birthday,',Name,'!',Message)
-elif Age % 10 == 3:
-    print('Happy',Age,'rd Birthday,',Name,'!',Message)
-else:
-    print('Happy',Age,'th Birthday,',Name,'!',Message)
-
-    for i in range(1,3):
+# Name = input('Who is the birthday person?')
+# Age = int(input('How old will he or she be this year?'))
+# Message = input('What is your personal message to him or her?')
+# if Age == 11 or Age == 12:
+#     print('Happy',Age,'th Birthday,',Name,'!',Message)
+# elif Age % 10 == 1 :
+#     print('Happy',Age,'st Birthday,',Name,'!',Message)
+# elif Age % 10 == 2:
+#     print('Happy',Age,'nd Birthday,',Name,'!',Message)
+# elif Age % 10 == 3:
+#     print('Happy',Age,'rd Birthday,',Name,'!',Message)
+# else:
+#     print('Happy',Age,'th Birthday,',Name,'!',Message)
+for i in range(1,3):
         print("I like chicken rice.")
