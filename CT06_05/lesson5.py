40207783b73004e3685b628712f3b498edee6fd7
javen2015 <zqlin15@gmail.com>
2025-02-09 08:36:01 +0000

    Feb 9, 2025, 8:36 AM

--- CT06_05/lesson5.py
+++ CT06_05/lesson5.py
@@ -1,4 +1,8 @@
 Name = input('Who is the birthday person?')
 Age = int(input('How old will he or she be this year?'))
 Message = input('What is your personal message to him or her?')
-print('Happy',Age,'th Birthday,',Name,'!',Message)
+
+if Age % 10 == 9 :
+    print('Happy',Age,'st Birthday,',Name,'!',Message)
+elif Age % 10 == 8:
+    print
