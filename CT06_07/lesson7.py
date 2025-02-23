67d0b72e2b36d4eed913520ab84b7de3841827ca
javen2015 <zqlin15@gmail.com>
2025-02-23 09:02:41 +0000

    Feb 23, 2025, 9:02 AM

--- CT06_07/lesson7.py
+++ CT06_07/lesson7.py
@@ -199,16 +199,6 @@
 
 
 
-Stars = int(input('How many rows stars are there?'))
-num = 1
-spaces=Stars-1
-
-for i in range(Stars):
-    num=num+2
-    spaces=spaces+1
-    print(' '* (Stars - i), '*' * num)
-
-
 
 
 Stars = int(input('How many rows stars are there?'))
