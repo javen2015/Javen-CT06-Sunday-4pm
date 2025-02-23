fcb28ec087b4338d5ea83f628e770a0c5ab9218b
javen2015 <zqlin15@gmail.com>
2025-02-23 08:30:51 +0000

    Feb 23, 2025, 8:30 AM

--- CT06_07/lesson7.py
+++ CT06_07/lesson7.py
@@ -1,13 +1,13 @@
-print("Hello from lesson 7")
+# print("Hello from lesson 7")
 
-score_one = 80
-score_two = 90
-score_three = 75
+# score_one = 80
+# score_two = 90
+# score_three = 75
 
-total = score_one + score_two + score_three
+# total = score_one + score_two + score_three
 
-average_score = total / 3
+# average_score = total / 3
 
-student_name = "Alex"
+# student_name = "Alex"
 
-print("Average score for " + student_name + " is: " + str(average_score))
+# print("Average score for " + student_name + " is: " + str(average_score))
