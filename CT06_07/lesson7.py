a643084bf33b261460ab8ba33b41b2b4225006fc
javen2015 <zqlin15@gmail.com>
2025-02-23 09:08:01 +0000

    Feb 23, 2025, 9:08 AM

--- CT06_07/lesson7.py
+++ CT06_07/lesson7.py
@@ -34,6 +34,8 @@
 
 
 
+for i in range(1,6):
+    num
 
 
 
@@ -48,28 +50,11 @@
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-no1=int(input("What is number #1?")) 
-no2=int(input("What is number #2?"))
-no3=int(input("What is number #3?"))
-no4=int(input("What is number #4?"))
-no5=int(input("What is number #5?"))
+# no1=int(input("What is number #1?")) 
+# no2=int(input("What is number #2?"))
+# no3=int(input("What is number #3?"))
+# no4=int(input("What is number #4?"))
+# no5=int(input("What is number #5?"))
 # print("The sum of the numbers is", str(no1+no2+no3+no4+no5))
 
 
