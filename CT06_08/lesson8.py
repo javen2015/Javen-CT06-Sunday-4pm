c55fe20e1e974068274a4d2368b6aff5f27ed720
javen2015 <zqlin15@gmail.com>
2025-03-02 08:19:22 +0000

    Mar 2, 2025, 8:19 AM

--- CT06_08/lesson8.py
+++ CT06_08/lesson8.py
@@ -1,9 +1,9 @@
-# Number1=int(input("What is number #1?")) 
-# Number2=int(input("What is number #2?"))
-# Number3=int(input("What is number #3?"))
-# Number4=int(input("What is number #4?"))
-# Number5=int(input("What is number #5?"))
-# print("The product of the numbers is", str()))
+Number1=int(input("What is number #1?")) 
+Number2=int(input("What is number #2?"))
+Number3=int(input("What is number #3?"))
+Number4=int(input("What is number #4?"))
+Number5=int(input("What is number #5?"))
+print("The product of the numbers is", str(Number1*Number2*Number3*Number4*Number5))
 
 
 
