1ff1f4be7d835b274e63da2d30234f24bc75f09e
javen2015 <zqlin15@gmail.com>
2025-03-02 08:51:01 +0000

    Mar 2, 2025, 8:51 AM

--- CT06_08/lesson8.py
+++ CT06_08/lesson8.py
@@ -26,10 +26,10 @@
 #     time.sleep(1)
 #     print(i)
 
-# import time
-# for i in range(10,0,-1):
-#     time.sleep(1)
-#     print(i)
+import time
+for i in range(10,0,-1):
+    time.sleep(1)
+    print(i)
 
 
 # import random
