9626849381bbad07a40b4958b27f88a000d94c03
javen2015 <zqlin15@gmail.com>
2025-03-02 09:09:48 +0000

    Mar 2, 2025, 9:09 AM

--- CT06_08/lesson8.py
+++ CT06_08/lesson8.py
@@ -66,7 +66,14 @@
 # else:
 #     print(False)
 
-
+import random
+no1=random.randint(0,50)
+no2=random.randint(0,50)
+useranswer=int(input("What is "+ str(no1)+" + "+ str(no2)+"?"))
+if useranswer==no1+no2:
+    print(True)
+else:
+    print(False)
 #Task 6 skipped
 
 # number=int(input("What is the number?"))
