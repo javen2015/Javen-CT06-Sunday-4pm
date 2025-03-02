d93b16846f3074d1668d16da10fc39fc327b34ab
javen2015 <zqlin15@gmail.com>
2025-03-02 08:04:16 +0000

    Mar 2, 2025, 8:04 AM

--- CT06_08/lesson8.py
+++ CT06_08/lesson8.py
@@ -1,2 +1,7 @@
-print("Hello from lesson 8")
+no1=int(input("What is number #1?")) 
+# no2=int(input("What is number #2?"))
+# no3=int(input("What is number #3?"))
+# no4=int(input("What is number #4?"))
+# no5=int(input("What is number #5?"))
+# print("The sum of the numbers is", str(no1+no2+no3+no4+no5))
 
