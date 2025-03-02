788530b1494038e880eae6eb3a51301acaac4f4b
javen2015 <zqlin15@gmail.com>
2025-03-02 08:50:40 +0000

    Mar 2, 2025, 8:50 AM

--- CT06_08/lesson8.py
+++ CT06_08/lesson8.py
@@ -20,11 +20,17 @@
 #     time.sleep(1)
 #     print(i)
 
-import time
-countdownnumber=int(input("What number do you want to count down from?"))
-for i in range(countdownnumber,0,-1):
-    time.sleep(1)
-    print(i)
+# import time
+# countdownnumber=int(input("What number do you want to count down from?"))
+# for i in range(countdownnumber,0,-1):
+#     time.sleep(1)
+#     print(i)
+
+# import time
+# for i in range(10,0,-1):
+#     time.sleep(1)
+#     print(i)
+
 
 # import random
 # print(random.random(0,6))
