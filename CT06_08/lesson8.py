eb8041aac14a1de49cab47a877ebd17ea7cc7492
javen2015 <zqlin15@gmail.com>
2025-03-02 08:51:41 +0000

    Mar 2, 2025, 8:51 AM

--- CT06_08/lesson8.py
+++ CT06_08/lesson8.py
@@ -27,9 +27,9 @@
 #     print(i)
 
 import time
-countingdownnumber=int(input("What number do you want to count down from?"))
+countingdownnumber=int(input("What minute do you want to count down from?"))
 for i in range(countingdownnumber,0,-1):
-    time.sleep(1)
+    time.sleep(60)
     print(i)
 
 
