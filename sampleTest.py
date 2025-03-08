f9d57842ab1ddbc31035233f8aaad67d53d48b35
javen2015 <zqlin15@gmail.com>
2025-03-08 00:50:09 +0000

    Mar 8, 2025, 12:50 AM

--- sampleTest.py
+++ sampleTest.py
@@ -1,2 +1,2 @@
 distancetravelled=int(input("How much distance have you travelled (km)?"))
-expressbusservicestaken=input("Have you taken any")
+expressbusservicestaken=input("Have you taken any express bus services?")
