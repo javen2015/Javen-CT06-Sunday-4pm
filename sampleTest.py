b4df0eec7070f9769f6fd927f1c2119a65dc75d8
javen2015 <zqlin15@gmail.com>
2025-03-08 01:21:37 +0000

    Mar 8, 2025, 1:21 AM

--- sampleTest.py
+++ sampleTest.py
@@ -1,8 +1,9 @@
 Fare=0
+distancetravelled2=0
 distancetravelled=int(input("How much distance have you travelled (km)?"))
 expressbusservicestaken=input("Have you taken any express bus services? Type yes or no.")
 if expressbusservicestaken == "yes":
     Fare=Fare+0.60
 if distancetravelled > 3.3 and distancetravelled < 6.2:
-    km=distancetravelled%3
-    
+    distancetravelled2 = 30
+ 
