62bf345203552d49ce4c1b31ee04a24e38efbc12
javen2015 <zqlin15@gmail.com>
2025-03-08 00:51:52 +0000

    Mar 8, 2025, 12:51 AM

--- sampleTest.py
+++ sampleTest.py
@@ -1,3 +1,5 @@
 Fare=0
 distancetravelled=int(input("How much distance have you travelled (km)?"))
 expressbusservicestaken=input("Have you taken any express bus services? Type yes or no.")
+if expressbusservicestaken == "yes":
+    f
