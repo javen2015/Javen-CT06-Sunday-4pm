98433e838e7cbc31edade77d8fa9a22bf9a8eb46
javen2015 <zqlin15@gmail.com>
2025-03-13 07:08:00 +0000

    Mar 13, 2025, 7:08 AM

---
+++ test20250313.py
@@ -0,0 +1,17 @@
+# Write a PYTHON program that help students to determine 
+# if they should go to school. You should prompt the user if 
+# they have the following flu symptoms:
+
+# Fever
+# Cough
+# Running nose
+# Sore throat
+
+# Your program should print
+
+# “Please go and see a doctor” if the user has any of such symptoms; or
+# “Please come into the class” if the user has no such symptoms.
+# A user is deemed to be having a fever if the body temperature is above 38°C.
+
+# You may assume that the user will type in either “yes” or “no” 
+# for the subsequent questions.
