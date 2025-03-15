f1dcaebfd52e339c4c6469fffc7f46cdf59d57ed
javen2015 <zqlin15@gmail.com>
2025-03-15 06:23:04 +0000

    Mar 15, 2025, 6:23 AM

--- CT07_1.py
+++ CT07_1.py
@@ -16,6 +16,7 @@ password=input("What is your password?")
 hasUpper=password.isupper()
 hasLower=password.islower()
 hasDigit=password.isdigit()
+print()
 if password.isalnum() and len(password)>7 and hasUpper and hasLower and hasDigit:#alphabets and numbers
     print("True")
 else:
