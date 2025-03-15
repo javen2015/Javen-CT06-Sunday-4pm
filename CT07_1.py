f2545238884881f7bc8e49f1a33ea03f1c826b4e
javen2015 <zqlin15@gmail.com>
2025-03-15 07:14:26 +0000

    Mar 15, 2025, 7:14 AM

--- CT07_1.py
+++ CT07_1.py
@@ -55,5 +55,5 @@ sentence="Hello World"
 sentence.split(" ")
 splitsentence=sentence[0][::-1]
 splitsentence[1][::-1]
-joinedsentence=(" ").join(sentence)
+joinedsentence=(" ").join(splitsentence)
 print(sentence)
