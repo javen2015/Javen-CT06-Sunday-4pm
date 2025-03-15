b13f51ecd1baf62391f7a26031e02229c260554b
javen2015 <zqlin15@gmail.com>
2025-03-15 06:12:06 +0000

    Mar 15, 2025, 6:12 AM

--- CT07_1.py
+++ CT07_1.py
@@ -3,16 +3,18 @@
 # print(list[1:3]) #list[start(inclusive):end(exclusive(-1))]
 
 
-name=input("What is your name?")
-if name.isalpha:#alphabet
-    print("True")
-else:
-    print("False")
-if name.isnumeric:#numbers
-    print("True")
-else:
-    print("False")
-if name.isalnum:#alphabetrs and numbers
-    print("True")
-else:
-    print("False")
+# name=input("What is your name?")
+# if name.isalpha:#alphabet
+#     print("True")
+# else:
+#     print("False")
+# if name.isnumeric:#numbers
+#     print("True")
+# else:
+#     print("False")
+# if name.isalnum:#alphabetrs and numbers
+#     print("True")
+# else:
+#     print("False")
+
+
