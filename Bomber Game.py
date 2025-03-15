aaf20f0c4091b1c8947af416e6ba0a4dec55febf
javen2015 <zqlin15@gmail.com>
2025-03-15 02:16:51 +0000

    Mar 15, 2025, 2:16 AM

--- Bomber Game.py
+++ Bomber Game.py
@@ -25,5 +25,4 @@ for i in range(1):
     print(grid[0]+"\n"+grid[1]+"\n"+grid[2]+"\n"+grid[3]+"\n"+grid[4]+"\n"+grid[5]+"\n"+grid[6]+"\n"+grid[7]+"\n"+grid[8])
     print("")
     answer=input("What position do you want to move to?")
-    playerPosition=answer-1
-    grid[]
+    grid[playerPosition]=""
