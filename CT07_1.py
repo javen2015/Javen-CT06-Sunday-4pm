1ad73c2f9978401dd3aa03975221b139d6e0d4e0
javen2015 <zqlin15@gmail.com>
2025-03-15 07:19:40 +0000

    Mar 15, 2025, 7:19 AM

--- CT07_1.py
+++ CT07_1.py
@@ -58,4 +58,4 @@ sentence="Hello World"
 # reversedsentence+=splitsentence[0][::-1]
 # joinedsentence=("").join(reversedsentence)
 # print(joinedsentence)
-print(sentence.reverse)
+print(sentence.reversed())
