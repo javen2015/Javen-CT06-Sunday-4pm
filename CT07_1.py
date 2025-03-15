39e5a0d666f0fea6b509d910d03c82aec7b81e02
javen2015 <zqlin15@gmail.com>
2025-03-15 06:08:21 +0000

    Mar 15, 2025, 6:08 AM

--- CT07_1.py
+++ CT07_1.py
@@ -1,3 +1,3 @@
-list=[1,10,9,8,7,20,16]
-print(sorted(list))
-print(list[1:3]) #list[start(inclusive):end(exclusive(-1))]
+# list=[1,10,9,8,7,20,16]
+# print(sorted(list))
+# print(list[1:3]) #list[start(inclusive):end(exclusive(-1))]
