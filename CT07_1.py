0fe734eda65ea1a865b7b3db915458077632fe17
javen2015 <zqlin15@gmail.com>
2025-03-15 06:15:45 +0000

    Mar 15, 2025, 6:15 AM

--- CT07_1.py
+++ CT07_1.py
@@ -16,7 +16,7 @@ password=input("What is your name?")
 hasUpper=password.isupper()
 hasLower=password.islower()
 
-if password.isalnum and len(password)==8:#alphabets and numbers
+if password.isalnum() and len(password)==8 and ha:#alphabets and numbers
     print("True")
 else:
     print("False")
