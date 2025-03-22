182fe79a851b84549cb6e35a53e40d15abaa1a15
javen2015 <zqlin15@gmail.com>
2025-03-22 06:49:32 +0000

    Mar 22, 2025, 6:49 AM

--- CT07_1.py
+++ CT07_1.py
@@ -102,8 +102,10 @@ for i in range(len(sales_data)):
     price.append(sales_data[i][2]+sales_data[i][1]*sales_data[i][2])
 if max(price)=="Apple":
     speech+="1. Apple"+str(max(price))
+    fruit="Apple"
 elif max(price)=="Banana":
     speech+="1. Banana"+str(max(price))
+    fruit="Banana"
 elif max(price)=="Orange":
     speech+="1. Orange"+str(max(price))
 elif max(price)=="Grapefruit":
