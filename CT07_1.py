9b32f13a39f5d15f060bafa6216f92679becb7be
javen2015 <zqlin15@gmail.com>
2025-03-22 07:23:59 +0000

    Mar 22, 2025, 7:23 AM

--- CT07_1.py
+++ CT07_1.py
@@ -178,6 +178,6 @@ passwordlist=[]
 while True:
     ui=input("Enter a s d or q")
     def a():
-        User=input("Username")
+        usename=input("Username")
         user
         passwordlist.append()
