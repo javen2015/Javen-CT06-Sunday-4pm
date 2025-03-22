c1af043311bfbf56a02f7df90a2235765085eade
javen2015 <zqlin15@gmail.com>
2025-03-22 01:52:29 +0000

    Mar 22, 2025, 1:52 AM

--- mastermind.py
+++ mastermind.py
@@ -1,7 +1,8 @@
 import random
 counter=0
+result=0
 mastermindnumber=[1,2,3,4]
-while not mastermindnumber[0]==mastermindnumber[1] and not mastermindnumber[1]==mastermindnumber[2] and not mastermindnumber[2] == mastermindnumber[3]:
+while result == all(x == a[0] for x in a):
     for i in range(4):
         number=random.randint(0,9)
         mastermindnumber[i]=number
