3eee0d70cdd6f3b8a8cf45e11af0a5919e37090e
javen2015 <zqlin15@gmail.com>
2025-03-22 07:25:33 +0000

    Mar 22, 2025, 7:25 AM

--- CT07_1.py
+++ CT07_1.py
@@ -174,6 +174,7 @@
 # print("Top 1 fruit: " + str(sales_data[top3[0]][1]))
 # print("Top 2 fruit: " + str(sales_data[top3[1]][1]))
 # print("Top 3 fruit: " + str(sales_data[top3[2]][1]))
+fullPasswordList[]
 passwordlist=[]
 while True:
     ui=input("Enter a s d or q")
