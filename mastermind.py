efbf1294c083e0d125528ea2483d5cfc2fc5f3e3
javen2015 <zqlin15@gmail.com>
2025-03-22 02:10:37 +0000

    Mar 22, 2025, 2:10 AM

--- mastermind.py
+++ mastermind.py
@@ -3,4 +3,4 @@ mastermindnumber=[1,2,3,4]
 for i in range(4):
     number=random.randint(0,9)
     mastermindnumber[i]=number
-guess=int(input(""))
+guess=int(input("Guess the number with each number seperated by a space."))
