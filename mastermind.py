a3331cc1a8a4c87415338742a498388d1790c8fc
javen2015 <zqlin15@gmail.com>
2025-03-22 02:06:45 +0000

    Mar 22, 2025, 2:06 AM

--- mastermind.py
+++ mastermind.py
@@ -2,11 +2,10 @@ import random
 counter=0
 result=0
 mastermindnumber=[1,2,3,4]
-for i in range(100):
-    for i in range(4):
-        number=random.randint(0,9)
-        mastermindnumber[i]=number
-    print(mastermindnumber)
-    counter+=1
-    result = all(x == mastermindnumber[0] for x in mastermindnumber)
+for i in range(4):
+    number=random.randint(0,9)
+    mastermindnumber[i]=number
+print(mastermindnumber)
+counter+=1
+result = all(x == mastermindnumber[0] for x in mastermindnumber)
 print(counter)
