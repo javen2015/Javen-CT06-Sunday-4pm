e2ca9b2009449f9bb9740f471e9a435e1f2a2d6c
javen2015 <zqlin15@gmail.com>
2025-03-22 01:45:39 +0000

    Mar 22, 2025, 1:45 AM

--- mastermind.py
+++ mastermind.py
@@ -1,7 +1,7 @@
 import random
 counter=0
 mastermindnumber=[]
-while not mastermindnumber[0]==mastermindnumber[1] and not mastermindnumber[2]==mastermindnumber[3] and not mastermindnumber[2] == mastermindnumber[3]:
+while not mastermindnumber[0]==mastermindnumber[1] and not mastermindnumber[1]==mastermindnumber[2] and not mastermindnumber[2] == mastermindnumber[3]:
     for i in range(4):
         number=random.randint(0,9)
         mastermindnumber.append(number)
