6d080832f0ef23f43bd451a2c4bbfb2d65df31b9
javen2015 <zqlin15@gmail.com>
2025-03-22 01:53:24 +0000

    Mar 22, 2025, 1:53 AM

--- mastermind.py
+++ mastermind.py
@@ -2,11 +2,11 @@ import random
 counter=0
 result=0
 mastermindnumber=[1,2,3,4]
-while result == all(x == mastermindnumber[0] for x in mas):
+while result == all(x == mastermindnumber[0] for x in mastermindnumber):
     for i in range(4):
         number=random.randint(0,9)
         mastermindnumber[i]=number
     print(mastermindnumber)
     counter+=1
-    result = all(x == a[0] for x in a)
+    result = all(x == mastermindnumber[0] for x in a)
 print(counter)
