a9281e6779c600881419523d3bbfb3fd89cbfe16
javen2015 <zqlin15@gmail.com>
2025-03-22 02:15:40 +0000

    Mar 22, 2025, 2:15 AM

--- mastermind.py
+++ mastermind.py
@@ -6,4 +6,4 @@ for i in range(4):
 guess=input("Guess the number with each number seperated by a space.")
 guessindividual=guess.split(" ")
 if guessindividual[0]==mastermindnumber[0]:
-    guessindividual[0]=""
+    guessindividual[0]="C"
