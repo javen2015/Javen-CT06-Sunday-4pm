0792349f81af78f1fb2e39b3e7da628ccbd90375
javen2015 <zqlin15@gmail.com>
2025-03-22 07:34:25 +0000

    Mar 22, 2025, 7:34 AM

--- CT07_1.py
+++ CT07_1.py
@@ -175,9 +175,9 @@ for i in range(3):
     top3.append(index_of_highest)
     price[index_of_highest] = 0
 print(top3)
-print("Top 1 fruit: " + str(sales_data[top3[0]][0] + [1]))
-print("Top 2 fruit: " + str(sales_data[top3[1]][0][1]))
-print("Top 3 fruit: " + str(sales_data[top3[2]][0][1]))
+print("Top 1 fruit: " + str(sales_data[top3[0]][0]) + str(sales_data[top3[0]][1]))
+print("Top 2 fruit: " + str(sales_data[top3[1]][0]))
+print("Top 3 fruit: " + str(sales_data[top3[2]][0]))
 
 # def a():
 #         n=input("Username")
