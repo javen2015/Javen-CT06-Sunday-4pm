841bb8c82de75ffeda7e7c59d49f6b50f099dc4a
javen2015 <zqlin15@gmail.com>
2025-03-22 06:25:11 +0000

    Mar 22, 2025, 6:25 AM

--- CT07_1.py
+++ CT07_1.py
@@ -81,10 +81,10 @@ splitui=str(ui).split(" ")
 for i in range(len(splitui)):
     reversedui=str(splitui[i])[::-1]  
     if reversedui==str(ui):
-        output=True
         counter+=1
 if counter==0:
     print("False")
+
 
  
 
