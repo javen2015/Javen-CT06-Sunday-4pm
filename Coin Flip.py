988fc8accb72d4fe4bc349d9aa0a66af5d9b3a26
javen2015 <zqlin15@gmail.com>
2025-03-22 02:48:55 +0000

    Mar 22, 2025, 2:48 AM

--- Coin Flip.py
+++ Coin Flip.py
@@ -2,8 +2,8 @@ import random
 heads=0
 tails=0
 for i in range(100):
-    number=random.randint(0,1)
-    if number == "0":
+    number=random.randint(0,2)
+    if number == "1":
         heads+=1
     else:
         tails+=1
