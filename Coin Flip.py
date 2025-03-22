41543ceb426bf02feb2aabf14a654d871792c748
javen2015 <zqlin15@gmail.com>
2025-03-22 06:04:05 +0000

    Mar 22, 2025, 6:04 AM

--- Coin Flip.py
+++ Coin Flip.py
@@ -3,10 +3,9 @@ heads=0
 tails=0
 for i in range(100):
     number=random.randint(0,1)
-#     if number == "0":
-#         heads+=1
-#     elif number=="1":
-#         tails+=1
-# print("Heads: "+str(heads))
-# print("Tails: "+str(tails))
-    print(number)
+    if number == "0":
+        heads+=1
+    elif number=="1":
+        tails+=1
+print("Heads: "+str(heads))
+print("Tails: "+str(tails))
