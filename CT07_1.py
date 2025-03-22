96ac7a6ff1188c1e0f4015ef377489fae0eebde8
javen2015 <zqlin15@gmail.com>
2025-03-22 07:11:07 +0000

    Mar 22, 2025, 7:11 AM

--- CT07_1.py
+++ CT07_1.py
@@ -156,10 +156,13 @@
 # print(highestSellingFruit[2])
 
 
-["Apple",50,1.99],
+sales_data=["Apple",50,1.99],
 ["Banana",40,0.99],
 ["Orange",30,2.99],
 ["]Grapefruit",25,4.99],
 ["Mange",20,3.99]
 price=[]
 top3=[]
+
+for item in sales_data:
+    prcie
