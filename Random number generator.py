b5d6506433e8b474252d76de371896328d4a0d30
javen2015 <zqlin15@gmail.com>
2025-03-29 06:54:52 +0000

    Mar 29, 2025, 6:54 AM

--- Random number generator.py
+++ Random number generator.py
@@ -5,5 +5,5 @@ number=random.randint(int(splitNumberRange[0]),int(splitNumberRange[1]))
 minusing=int(int(splitNumberRange[1])+1)-int(splitNumberRange[0])
 dividingNumber=100/minusing
 rounded=round(dividingNumber),2
-print("There is a "+str()+"% chance of each number generating.")
+print("There is a "+str(rounded)+"% chance of each number generating.")
 print(number)
