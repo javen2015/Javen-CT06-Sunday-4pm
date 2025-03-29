ff3bc5252e3b2928bd5a25e1f6ba0d6b4d4fe11a
javen2015 <zqlin15@gmail.com>
2025-03-29 07:27:41 +0000

    Mar 29, 2025, 7:27 AM

--- CT07_3.py
+++ CT07_3.py
@@ -14,6 +14,7 @@
 # When you put ####.split() python will refer it to no space or have space. *IMPORTANT*
 
 import turtle
+Guess
 window = turtle.Screen()
 window.bgcolor("forestgreen")
 window.setup(600,600)
