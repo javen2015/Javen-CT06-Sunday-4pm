253a59259b0b6f6290cd3e958f458d27413ca7cb
javen2015 <zqlin15@gmail.com>
2025-03-29 06:25:38 +0000

    Mar 29, 2025, 6:25 AM

--- Random number generator.py
+++ Random number generator.py
@@ -2,5 +2,5 @@ import random
 numberRange=input("What is the number range? Type the minimum number and the maximum number seperated by a space.")
 splitNumberRange=numberRange.split(" ")
 number=random.randint(int(splitNumberRange[0]),int(splitNumberRange[1]))
-print("There is a "+str(100/len(splitNumberRange)+"% chance of each number generating.")
+print("There is a "+str(100/len(splitNumberRange))+"% chance of each number generating.")
 print(number)
