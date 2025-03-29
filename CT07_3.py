85fef8be9fd5f299a4da8b1fc3e4921603151083
javen2015 <zqlin15@gmail.com>
2025-03-29 06:17:22 +0000

    Mar 29, 2025, 6:17 AM

--- CT07_3.py
+++ CT07_3.py
@@ -4,13 +4,13 @@ ui=input("What has to be broken before you can use it?")
 uiupper=ui.upper()
 for i in ui:
     if i ==" ":
-        haveSpace==""
+        haveSpace==True
 uisplit=uiupper.split(" ")
 for i in uisplit:
     if i =="EGG":
         isCorrect=True
         break
-if isCorrect=="True":
+if isCorrect:
     print("Correct!Well done!")
 else:
     print("You got it wrong:(")
