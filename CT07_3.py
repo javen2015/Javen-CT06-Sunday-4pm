27176dc320917d99f1cbb710a51389c907898630
javen2015 <zqlin15@gmail.com>
2025-03-29 06:34:57 +0000

    Mar 29, 2025, 6:34 AM

--- CT07_3.py
+++ CT07_3.py
@@ -1,14 +1,14 @@
-haveSpace=False
-isCorrect=False
-ui=input("What has to be broken before you can use it?")
-uiupper=ui.upper()
-uisplit=uiupper.split()
-for i in uisplit:
-    if i =="EGG":
-        isCorrect=True
-        break
-if isCorrect:
-    print("Correct!Well done!")
-else:
-    print("You got it wrong.")
+# haveSpace=False
+# isCorrect=False
+# ui=input("What has to be broken before you can use it?")
+# uiupper=ui.upper()
+# uisplit=uiupper.split()
+# for i in uisplit:
+#     if i =="EGG":
+#         isCorrect=True
+#         break
+# if isCorrect:
+#     print("Correct!Well done!")
+# else:
+#     print("You got it wrong.")
 # When you put ####.split() python will refer it to no space or have space.
