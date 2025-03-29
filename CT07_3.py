b45bf64022aca3133ed44165f5b5f6b0ea9fdd3c
javen2015 <zqlin15@gmail.com>
2025-03-29 06:18:17 +0000

    Mar 29, 2025, 6:18 AM

--- CT07_3.py
+++ CT07_3.py
@@ -4,9 +4,8 @@ ui=input("What has to be broken before you can use it?")
 uiupper=ui.upper()
 for i in ui:
     if i ==" ":
-        
-        
         haveSpace=True
+        break
 uisplit=uiupper.split(" ")
 for i in uisplit:
     if i =="EGG":
