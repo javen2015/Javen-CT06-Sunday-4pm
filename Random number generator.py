a459f6c850306e58b9cac2b3261e9c9d06aaa6dc
javen2015 <zqlin15@gmail.com>
2025-03-29 06:49:30 +0000

    Mar 29, 2025, 6:49 AM

--- Random number generator.py
+++ Random number generator.py
@@ -2,6 +2,6 @@ import random
 numberRange=input("What is the number range? Type the minimum number and the maximum number seperated by a space.")
 splitNumberRange=numberRange.split(" ")
 number=random.randint(int(splitNumberRange[0]),int(splitNumberRange[1]))
-minusing=int(int(splitNumberRange[1])+1)-1
+minusing=int(int(splitNumberRange[1])+1)-int(splitNumberRange[0])
 print("There is a "+str(100/minusing)+"% chance of each number generating.")
 print(number)
