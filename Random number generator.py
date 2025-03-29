25df9ee70dffc6d42a634a1cff5c01da222482c1
javen2015 <zqlin15@gmail.com>
2025-03-29 06:04:39 +0000

    Mar 29, 2025, 6:04 AM

--- Random number generator.py
+++ Random number generator.py
@@ -1,4 +1,4 @@
 import random
 numberRange=int(input("What is the number range? Type the minimum number and the maximum number seperated by a space."))
 splitNumberRange=numberRange.split(" ")
-number=random.randint()
+number=random.randint(splitNumberRange[0],split)
