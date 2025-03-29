42a704979f9bf9495cf5fb2e7adf388342127ead
javen2015 <zqlin15@gmail.com>
2025-03-29 07:05:30 +0000

    Mar 29, 2025, 7:05 AM

--- CT07_3.py
+++ CT07_3.py
@@ -17,8 +17,8 @@ import turtle as t
 window = t.Screen()
 window.bgcolor("forestgreen")
 window.setup(600,600)
+t.penup()
 t.seth(0)
-t.penup
 t.goto(-300,250)
 for i in range(-300,300,25):
     t.setx(i)
