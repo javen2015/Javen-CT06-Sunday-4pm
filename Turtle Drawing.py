40d68e655dc234d859249bb93ee23747f2fda9c7
javen2015 <zqlin15@gmail.com>
2025-03-30 02:49:14 +0000

    Mar 30, 2025, 2:49 AM

--- Turtle Drawing.py
+++ Turtle Drawing.py
@@ -3,12 +3,5 @@ window = turtle.Screen()
 window.bgcolor("forestgreen")
 window.setup(600,600)
 t = turtle.Turtle()
-t.shape("square")
-t.penup()
-t.seth(0)
-t.goto(-300,250)
-for i in range(-300,300,25):
-    t.setx(i)
-    t.stamp()
-    t.penup()
+
 window.mainloop()
