c67726ff9ee0bcd41eb82b185584be4500d760f2
javen2015 <zqlin15@gmail.com>
2025-04-05 07:22:02 +0000

    Apr 5, 2025, 7:22 AM

--- CT7_10.py
+++ CT7_10.py
@@ -1,17 +1,18 @@
-import turtle as t
-window = t.Screen()
-window.bgcolor("cyan")
-window.setup(width=600,height=400)
-t=t.Turtle()
-t.shape("turtle")
-t.fillcolor("orange")
-t.seth(0)
-t.pendown()
-def turtle():
-    t.seth(0)
-    for i in range(2):
-        t.forward(100)
-        t.right(90)
-        t.forward(50)
-        t.right(90)
-window.mainloop()
+# import turtle as t
+# window = t.Screen()
+# window.bgcolor("cyan")
+# window.setup(width=600,height=400)
+# t=t.Turtle()
+# t.shape("turtle")
+# t.fillcolor("orange")
+# t.seth(0)
+# t.pendown()
+# def turtle():
+#     t.seth(0)
+#     for i in range(2):
+#         t.forward(100)
+#         t.right(90)
+#         t.forward(50)
+#         t.right(90)
+# window.mainloop()
+
