576621eea805ee53908f6a6be704c21002dbf20f
javen2015 <zqlin15@gmail.com>
2025-04-12 07:04:03 +0000

    Apr 12, 2025, 7:04 AM

--- myOwnTicTacToe.py
+++ myOwnTicTacToe.py
@@ -2,9 +2,21 @@ def initialiseBoard():
    grid=[]
    row=[]
    for i in range(3):
-      row=[]
-      row.append(" ")
-   grid.append(row)
+      for i in range(3):
+         row=[]
+         row.append(" ")
+         grid.append(row)
    return(grid)
-def normalGame():
-   initialiseBoard()
+grid=initialiseBoard()
+def printGrid(userAnswer):
+   cellnumber=1   
+   for i in range(3):
+      for i in range(3):
+         if cellnumber==userAnswer:
+            print(" | X ",end="")
+         else:
+            print(" | "+str(cellnumber)+" ",end="")
+         cellnumber+=1
+      print("\n---------------")
+printGrid(3)
+
