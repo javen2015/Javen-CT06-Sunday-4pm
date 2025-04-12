ebd766bb7e3dbb49d0a1a36a6fb2e310be8b805c
javen2015 <zqlin15@gmail.com>
2025-04-12 07:30:50 +0000

    Apr 12, 2025, 7:30 AM

---
+++ myOwnTicTacToe.py
@@ -0,0 +1,26 @@
+
+def initialiseBoard():
+   grid=[]
+   for i in range(3):
+      row=[]
+      for i in range(3):
+         row.append(" ")
+      grid.append(row)
+   return(grid)
+
+def printGrid(grid):
+   cellnumber=1   
+   for row in range(3):
+      for cell in range(3):
+         if grid[row][cell]!=" ":
+            print(" | "+grid[row][cell]+ " ",end="")
+         else:
+            print(" | "+str(cellnumber)+" ",end="")
+         cellnumber+=1
+      print("\n---------------")
+
+grid=initialiseBoard()
+questions = ["Player 1(X)","Player 2(0)"]
+for turn in range(9):
+   userChoice = input(questions[turn%2]+"Please key in your choice.")
+   printGrid(grid)
