4ccde619c27d668fbc514cb43304a1cc7f650a4a
javen2015 <zqlin15@gmail.com>
2025-04-12 06:08:26 +0000

    Apr 12, 2025, 6:08 AM

--- dice.py
+++ dice.py
@@ -3,6 +3,7 @@ diceNumber=r.randint(1,6)
 def convertOppositeSide(numberToConvert):
     if numberToConvert == 1:
         oppositeSideNumber=6
-        
+    elif numberToConvert == 2:
+        oppositeSideNumber=6
 guess=int("Guess the number.")
 print(diceNumber)
