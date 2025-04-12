4e5e10ccbc1ace84cf5f090dcc4fd55a0d9b958c
javen2015 <zqlin15@gmail.com>
2025-04-12 06:45:45 +0000

    Apr 12, 2025, 6:45 AM

--- tic-tac-toe.py
+++ tic-tac-toe.py
@@ -2,8 +2,9 @@ def initialiseBoard():
    grid=[]
    row=[]
    for i in range(3):
-      row=[]
-      row.append(" ")
+      for i in range(3):
+         row=[]
+         row.append(" ")
       grid.append(row)
    return(grid)
 grid=initialiseBoard()
