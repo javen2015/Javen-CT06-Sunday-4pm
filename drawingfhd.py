faeffd477f626b0defa68c1926c4c5ad4aad632c
javen2015 <zqlin15@gmail.com>
2025-04-19 06:23:45 +0000

    Apr 19, 2025, 6:23 AM

--- drawingfhd.py
+++ drawingfhd.py
@@ -1,7 +1,8 @@
 while True:
     stars=10
+    spaces=0
     num=0
-    for i in range(Stars):
+    for i in range(stars):
         num=num+2
         spaces=spaces+1
         print(' '* (Stars - i), '*' * num)
