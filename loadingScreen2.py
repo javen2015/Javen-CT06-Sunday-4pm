d3407d2fd3a919ada9b5569f8de0ac0cfef97684
javen2015 <zqlin15@gmail.com>
2025-04-26 05:58:20 +0000

    Apr 26, 2025, 5:58 AM

--- loadingScreen2.py
+++ loadingScreen2.py
@@ -1,2 +1,16 @@
+import time
 for i in range(10):
-    print
+    for i in range(1):
+        stars=100
+        spaces=100
+        num=200
+        for i in range(stars):
+            print(' '* (stars - i), '*' * num)
+            num=num-2
+            spaces=spaces-1
+            time.sleep(0.01)
+        for i in range(stars):
+            print(' '* (stars - i), '*' * num)
+            num=num+2
+            spaces=spaces+1
+            time.sleep(0.01)
