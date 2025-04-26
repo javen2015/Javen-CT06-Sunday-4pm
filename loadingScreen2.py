6ed245ce61e9ba55c7fe2b77ed5a8e0596cd1ab4
javen2015 <zqlin15@gmail.com>
2025-04-26 06:05:37 +0000

    Apr 26, 2025, 6:05 AM

--- loadingScreen2.py
+++ loadingScreen2.py
@@ -8,7 +8,7 @@ for i in range(10):
         num=num+2
         spaces=spaces+1
         time.sleep(0.01)
-
+    spaces=7
     for i in range(stars):
         print(' '* (stars + i), '*' * num)
         num=num-2
