2f8165a7d6dde320c59564562df548d3e5116eb7
javen2015 <zqlin15@gmail.com>
2025-05-10 04:36:40 +0000

    May 10, 2025, 4:36 AM

--- testtest.py
+++ testtest.py
@@ -1,6 +1,6 @@
 carNum=input("")
 condition=0
-if carNum[0].isalpha and carNum[0].isupper:
+if carNum[0].isalpha and carNum[0].isupper and carNum[1].isalpha and carNum[0].isupper:
     condition=True
 
 if condition:
