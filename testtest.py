3eae86b40bc116983e31cf26e012a44172ac626a
javen2015 <zqlin15@gmail.com>
2025-05-10 04:36:07 +0000

    May 10, 2025, 4:36 AM

--- testtest.py
+++ testtest.py
@@ -1,8 +1,8 @@
 carNum=input("")
+condition=0
 if carNum[0].isalpha and carNum[0].isupper:
-    condition=True
-else:
-    condition=False
+    condition+=1
+
 if condition:
     print("Valid")
 else:
