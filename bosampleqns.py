6f6a773333c5a69ef4046e0c6f9ff5018f557dd0
javen2015 <zqlin15@gmail.com>
2025-05-10 05:02:23 +0000

    May 10, 2025, 5:02 AM

---
+++ bosampleqns.py
@@ -0,0 +1,17 @@
+# Question:
+
+# The Singapore Mini-Car Club assigns a registration ID to each toy car. The format of the ID is as follows:
+
+# It starts with exactly 2 capital letters, followed by 3 to 4 digits, and ends with 1 capital letter.
+
+# Write a Python function that checks if a given registration ID is valid based on this format.
+
+# Rules:
+
+# The first two characters must be capital letters (A–Z).
+
+# The next 3 or 4 characters must be digits (0–9).
+
+# The final character must be a capital letter (A–Z).
+
+# If the registration ID is valid, return "Valid"; otherwise, return "Invalid".
