3028d4d135acc2dd8f8f2c319c8c1077e0a9bd47
javen2015 <zqlin15@gmail.com>
2025-05-10 05:08:51 +0000

    May 10, 2025, 5:08 AM

--- idod.py
+++ idod.py
@@ -1,2 +1,6 @@
 carNum=input("")
 condition=False
+if len(carNum)==6:
+    if carNum[0].isupper() and carNum[1].isupper():
+        if carNum[2].isdigit() and carNum[3].isdigit() and carNum[4].isdigit():
+            if carNum[5].isalpha()
