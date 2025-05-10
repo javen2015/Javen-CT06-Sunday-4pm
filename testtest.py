3cf8e09c26d3da2a840c10e9348dd6ad2205429b
javen2015 <zqlin15@gmail.com>
2025-05-10 04:40:16 +0000

    May 10, 2025, 4:40 AM

--- testtest.py
+++ testtest.py
@@ -1,9 +1,8 @@
 carNum=input("")
-condition=0
 if carNum[0].isalpha and carNum[0].isupper and carNum[1].isalpha and carNum[1].isupper and carNum[2].isdigit and carNum[3].isdigit and carNum[2].isdigit and carNum[4].isalpha and carNum[4].isupper :
     condition=True
 else:
-    
+    condition=False
 if condition:
     print("Valid")
 else:
